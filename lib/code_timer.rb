b57d30099213a67d9441d444ea5367888c37c6e0
Will Howes <williameldenhowes@gmail.com>
2019-09-17 15:04:32 +0100

    code accepts arrays and functions as arguments

--- lib/code_timer.rb
+++ lib/code_timer.rb
@@ -5,5 +5,5 @@ def code_timer(code)
   code
   end_time = Time.now
   time_taken = end_time - start_time
-  "Time taken: #{time_taken}"
+  p "Time taken: #{time_taken}"
 end
--- spec/code_timer_spec.rb
+++ spec/code_timer_spec.rb
@@ -3,6 +3,13 @@ require 'code_timer'
 describe 'code_timer' do
   it 'gives the time taken to run the function given a blank array' do
     expect(code_timer('a')).to include("Time taken: ")
+  end
 
+  it 'gives the time taken given an array' do
+    expect(code_timer('[a,a,a,a,a,a,a,a]')).to include("Time taken: ")
+  end
+
+  it 'gives the time taken given a function' do
+    expect(code_timer(['a','b'].reverse)).to include("Time taken: ")
   end
 end
