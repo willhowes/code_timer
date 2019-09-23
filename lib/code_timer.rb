d581db1cd72e8dd8768847ff5be787b120a01327
eldenhowes <williameldenhowes@gmail.com>
2019-09-23 10:44:34 +0100

    code_timer method correctly handles the .last method in a block

--- lib/code_timer.rb
+++ lib/code_timer.rb
@@ -4,14 +4,14 @@ SIZE_OF_ARRAYS = [50000, 100000, 150000, 200000, 250000, 300000,
   350000, 400000, 450000, 500000, 550000, 600000, 650000, 700000,
   750000, 800000, 850000, 900000, 950000, 10000000]
 
-def code_timer(block_of_code)
+def code_timer_for_arrays(block_of_code)
   results = []
   (SIZE_OF_ARRAYS).each do |size|
     array_to_test = generate_array_integers(size)
-    start_time = Time.now.nsec
+    start_time = Time.now
     block_of_code.call(array_to_test)
-    end_time = Time.now.nsec
-    time_taken = end_time - start_time
+    end_time = Time.now
+    time_taken = ((end_time - start_time) * 100000.0)
     results << [size, time_taken]
   end
   results
@@ -25,13 +25,13 @@ end
 def generate_array_string(size)
   array = []
   size.times { array << generate_string(rand(1)) }
-  array
+  array.shuffle
 end
 
 def generate_array_integers(size)
   array = []
   size.times { array << rand(0..9) }
-  array
+  array.shuffle
 end
 
 def printed_results(results)
@@ -42,4 +42,10 @@ def printed_results(results)
 end
 
 array_reverse = Proc.new { |array| array.reverse }
-printed_results(code_timer(array_reverse))
+printed_results(code_timer_for_arrays(array_reverse))
+
+array_last = Proc.new { |array| array.last }
+printed_results(code_timer_for_arrays(array_last))
+
+array_shuffle = Proc.new { |array| array.shuffle}
+printed_results(code_timer_for_arrays(array_shuffle))
--- spec/code_timer_spec.rb
+++ spec/code_timer_spec.rb
@@ -1,13 +1,18 @@
 require 'code_timer'
 
-describe 'code_timer_reverse' do
+describe 'code_timer_for_arrays_reverse' do
   it 'gives code times for using reverse method on increased sizes of arrays' do
     array_reverse = Proc.new { |array| array.reverse }
-    expect(code_timer(array_reverse).last[1]).to be > code_timer(array_reverse).first[1]
+    expect(code_timer_for_arrays(array_reverse).last[1]).to be > code_timer_for_arrays(array_reverse).first[1]
   end
 
   it 'gives code times for the last method on increasing size of arrays' do
     array_last = Proc.new { |array| array.last }
-    expect(code_timer(array_last).last[1]).to be > code_timer(array_last).first[1]
+    expect(code_timer_for_arrays(array_last).last[1]).to be >= code_timer_for_arrays(array_last).first[1]
+  end
+
+  it 'gives code times for the shuffle method on increasing size of arrays' do
+    array_shuffle = Proc.new { |array| array.shuffle }
+    expect(code_timer_for_arrays(array_shuffle).last[1]).to be >= code_timer_for_arrays(array_shuffle).first[1]
   end
 end
