0211e524020f6e452c4dc3a417427485b7201f18
eldenhowes <williameldenhowes@gmail.com>
2019-09-23 09:53:16 +0100

    code_timer can accept the last method as a block of code

--- spec/code_timer_spec.rb
+++ spec/code_timer_spec.rb
@@ -1,8 +1,13 @@
 require 'code_timer'
 
 describe 'code_timer_reverse' do
-  it 'gives code times for reverse one array' do
+  it 'gives code times for using reverse method on increased sizes of arrays' do
     array_reverse = Proc.new { |array| array.reverse }
-    expect(code_timer(array_reverse).last[1]).to be > code_timer(array_reverse).first[1] 
+    expect(code_timer(array_reverse).last[1]).to be > code_timer(array_reverse).first[1]
+  end
+
+  it 'gives code times for the last method on increasing size of arrays' do
+    array_last = Proc.new { |array| array.last }
+    expect(code_timer(array_last).last[1]).to be > code_timer(array_last).first[1]
   end
 end
