154a97203568fd96ea91ac5f4ca82ac6fa176e99
eldenhowes <williameldenhowes@gmail.com>
2019-09-24 10:49:17 +0100

    Refactored to make the put the code timer into a module so it can be used across other files

--- lib/code_timer.rb
+++ lib/code_timer.rb
@@ -1,54 +1,58 @@
 require 'time'
 require 'spreadsheet'
 
-SIZE_OF_ARRAYS = [50000, 100000, 150000, 200000, 250000, 300000,
-  350000, 400000, 450000, 500000, 550000, 600000, 650000, 700000,
-  750000, 800000, 850000, 900000, 950000, 1000000]
+module CodeTimer
 
-def code_timer_for_arrays(block_of_code)
-  results = []
-  (SIZE_OF_ARRAYS).each do |size|
-    array_to_test = generate_array_integers(size)
-    start_time = Time.now
-    block_of_code.call(array_to_test)
-    end_time = Time.now
-    time_taken = ((end_time - start_time) * 100000.0)
-    results << [size, time_taken]
-  end
-  results
-end
+  SIZE_OF_ARRAYS = [50000, 100000, 150000, 200000, 250000, 300000,
+    350000, 400000, 450000, 500000, 550000, 600000, 650000, 700000,
+    750000, 800000, 850000, 900000, 950000, 1000000]
 
-def generate_string(number)
-  charset = Array('A'..'Z') + Array('a'..'z') + Array(1..9)
-  Array.new(number) { charset.sample }.join
-end
-
-def generate_array_string(size)
-  array = []
-  size.times { array << generate_string(rand(1)) }
-  array.shuffle
-end
-
-def generate_array_integers(size)
-  array = []
-  size.times { array << rand(0..9) }
-  array.shuffle
-end
-
-def printed_results(results)
-  puts "TIME OF CODE RESULTS:"
-  results.each_with_index do |result, index|
-    puts "#{result[0]}: #{result[1]}\n"
-  end
-end
-
-def save_to_spreadsheet(results, file_name)
-  book = Spreadsheet::Workbook.new
-  book.create_worksheet :name => 'new_worksheet'
-
-  results.each_with_index do |result, index|
-    book.worksheet(0).insert_row(index, result)
+  def code_timer_for_arrays(block_of_code)
+    results = []
+    (SIZE_OF_ARRAYS).each do |size|
+      array_to_test = generate_array_integers(size)
+      start_time = Time.now
+      block_of_code.call(array_to_test)
+      end_time = Time.now
+      time_taken = ((end_time - start_time) * 100000.0)
+      results << [size, time_taken]
+    end
+    results
   end
 
-  book.write "#{file_name}.xls"
+  def generate_string(number)
+    charset = Array('A'..'Z') + Array('a'..'z') + Array(1..9)
+    Array.new(number) { charset.sample }.join
+  end
+
+  def generate_array_string(size)
+    array = []
+    size.times { array << generate_string(rand(1)) }
+    array.shuffle
+  end
+
+  def generate_array_integers(size)
+    array = []
+    size.times { array << rand(0..9) }
+    array.shuffle
+  end
+
+  def printed_results(results)
+    puts "TIME OF CODE RESULTS:"
+    results.each_with_index do |result, index|
+      puts "#{result[0]}: #{result[1]}\n"
+    end
+  end
+
+  def save_to_spreadsheet(results, file_name)
+    book = Spreadsheet::Workbook.new
+    book.create_worksheet :name => 'new_worksheet'
+
+    results.each_with_index do |result, index|
+      book.worksheet(0).insert_row(index, result)
+    end
+
+    book.write "#{file_name}.xls"
+  end
+  
 end
--- spec/code_timer_spec.rb
+++ spec/code_timer_spec.rb
@@ -1,23 +1,24 @@
 require 'code_timer'
+include CodeTimer
 
 describe 'code_timer_for_arrays_reverse' do
   it 'gives code times for using reverse method on increased sizes of arrays' do
     array_reverse = Proc.new { |array| array.reverse }
-    expect(code_timer_for_arrays(array_reverse).last[1]).to be > code_timer_for_arrays(array_reverse).first[1]
+    expect(CodeTimer.code_timer_for_arrays(array_reverse).last[1]).to be > code_timer_for_arrays(array_reverse).first[1]
   end
 
   it 'gives code times for the last method on increasing size of arrays' do
     array_last = Proc.new { |array| array.last }
-    expect(code_timer_for_arrays(array_last).last[1]).to be >= code_timer_for_arrays(array_last).first[1]
+    expect(CodeTimer.code_timer_for_arrays(array_last).last[1]).to be >= code_timer_for_arrays(array_last).first[1]
   end
 
   it 'gives code times for the shuffle method on increasing size of arrays' do
     array_shuffle = Proc.new { |array| array.shuffle }
-    expect(code_timer_for_arrays(array_shuffle).last[1]).to be >= code_timer_for_arrays(array_shuffle).first[1]
+    expect(CodeTimer.code_timer_for_arrays(array_shuffle).last[1]).to be >= code_timer_for_arrays(array_shuffle).first[1]
   end
 
   it 'gives code times for the sort method on increasing size of arrays' do
     array_sort = Proc.new { |array| array.sort }
-    expect(code_timer_for_arrays(array_sort).last[1]).to be >= code_timer_for_arrays(array_sort).first[1]
+    expect(CodeTimer.code_timer_for_arrays(array_sort).last[1]).to be >= code_timer_for_arrays(array_sort).first[1]
   end
 end
